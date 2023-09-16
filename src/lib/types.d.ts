f24560e5751efd896f6b7cc97c803cef8d6565cc
Tomás <t.diaz.soto@gmail.com>
2023-09-16 02:24:55 -0300

    feat(/lib/types.d.ts): add  Exercises and Days types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,13 +1,31 @@
+import { Days } from "./consts"
 
 export type InputProps = {
   type: string,
-  placeholder:string
+  placeholder:string | number
 }
 
 export type ButtonProps = {
   type:ButtonHTMLAttributes<HTMLButtonElement>
   text:string
   clickHandler:(e:ClickEvent)=> void
+}
+
+export type  ExerciseType = {
+  name: string,
+  sets: number,
+  repetitions:number
+}
+
+export type ExerciseProps = {
+  exercise:ExerciseType
+}
+
+export type ExerciseList = ExerciseType[]
+
+export type DayProps = {
+  exercises: ExerciseList
+  day: (typeof Days)[keyof typeof Days]
 }
 
 export type ClickEvent = React.MouseEvent<HTMLButtonElement>
