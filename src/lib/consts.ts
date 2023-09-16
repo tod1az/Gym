91b1a80dba2e7724cbbd9bffa68708cd8714204e
Tomás <t.diaz.soto@gmail.com>
2023-09-16 02:23:33 -0300

    feat(/lib/types.d.ts): add Days const

---
+++ src/lib/consts.ts
@@ -0,0 +1,9 @@
+export const Days = {
+  MONDAY:"monday",
+  TUESDAY:"tuesday",
+  WEDNESDAY:"wednesday",
+  THURSDAY:"thursday",
+  FRIDAY:"friday",
+  SATURDAY:"saturday",
+  SUNDAY:"sunday"
+}as const
